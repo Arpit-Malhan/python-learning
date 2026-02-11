3500f4b530b75d5703237f1f694bc365ae9d1983
Arpit Malhan <arpitmalhan@192.168.1.24>
2026-02-11 21:51:02 +0530

    Day 1: If-Else, Calculator, Grade System

---
+++ day1_basics.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+goal = input("What do you want to become? ")
+
+print("\n----- Your Profile -----")
+print("Name:", name)
+print("Age:", age)
+print("Future Goal:", goal)
+print(name, "will become a successful", goal, "🚀")
+
+print("\n-----VOID MEMEBERSSHIP-----")
+print("\n ----GRANT PERMISSSION----")
+print("Name:", name)
+print("password: ********")
